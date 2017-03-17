daaef5b30d06d09580238ecc5f49d9c77bec9218
Sokmean Nou <sokmean.nou@gmail.com>
2017-03-17 18:00:46 -0400

    footer init

---
+++ browser/react/components/DashFooter.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="page-footer">
+      <div className="container">
+        <div className="row">
+          <div className="col s1 offset-2">
+            <i className="fa fa-user" />
+          </div>
+        </div>
+      </div>
+    </footer>
+  )
+}
