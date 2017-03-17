ac1af369fabe656dfb1ce31986d4a8eab5c85b62
Maria Xia <maria.xia@gmail.com>
2017-03-17 17:20:27 -0400

    feat(react): index.html is now in react

---
+++ browser/react/components/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Grid from './Grid.js';
+
+class App extends React.Component {
+
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Constantinople</h3>
+        <Grid />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ browser/react/components/Grid.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import Cell from './Cell.js';
+
+class Grid extends React.Component {
+
+  render() {
+    let rows = [0, 1, 2, 3];
+    let columns = [0, 1, 2];
+
+    return (
+      <table>
+        <tbody>
+          {
+            rows.map(row =>
+              <tr key={row} id={`row-${row}`}>
+                {
+                  columns.map(column =>
+                    <Cell key={column} id={`${column},${row}`} />
+                  )
+                }
+              </tr>
+            )
+          }
+        </tbody>
+      </table>
+    )
+  }
+}
+
+export default Grid;
