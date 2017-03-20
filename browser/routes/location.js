d078f9ce8962b0c4f8879c702f0302af14a5c742
Sokmean Nou <sokmean.nou@gmail.com>
2017-03-20 17:45:44 -0400

    init location

---
+++ browser/routes/location.js
@@ -0,0 +1,7 @@
+'use strict';
+
+import React from 'react';
+
+export const maxGood = (playerId, good) => {
+  axios.put(`api/location/warehouse/${ playerId }/${ goodType }`)
+}
--- server/api/location.js
+++ server/api/location.js
@@ -1,10 +1,21 @@
+const admin = require('firebase-admin');
+const db = admin.database();
+const GameRef = db.ref('games');
 const router = module.exports = require('express').Router();
-
 /**
  * Location routes
  * ...api/location/...
  */
 
-// FILL ME IN
+ // Warehouse - Depending on the goodType, player will get max amount
 
-// router.get('/', (req, res, next) => {...})
+router.put('/warehouse/:playerId/:goodType', (req, res, next) => {
+  console.log('req.params', req.params);
+  const player = GameRef.child('merchants').child(`${req.params.playerId}`)
+  const good = player.child(`${req.params.goodType}`)
+  const wbSize = player.child('wheelbarrowSize')
+  wbSize.once('value').then(snap => {
+    good.set(snap.val());
+    next();
+  })
+})
