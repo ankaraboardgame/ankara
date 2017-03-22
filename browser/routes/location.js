cc1df263923b4e86d5f0f1063bd108ad06d3c1e9
Sokmean Nou <sokmean.nou@gmail.com>
2017-03-22 11:28:15 -0400

    updates(locations): axios routes

--- browser/routes/location.js
+++ browser/routes/location.js
@@ -4,41 +4,41 @@ import React from 'react';
 
 
 // 1. Wainwright -- Front end to check if the wheelbarrow is already max size
-export const actionBuyWbExt = () => {
-  axios.post(`api/location/wainwright`)
+export const actionBuyWbExt = (gameId, playerId) => {
+  axios.post(`api/game/${gameId}/player/${playerId}/location/wainwright`)
 }
 
 // 2. Warehouse Action: Max Good
-export const actionMaxGood = (goodType) => {
-  axios.post(`api/location/warehouse/${ goodType }`)
+export const actionMaxGood = (gameId, playerId, goodType) => {
+  axios.post(`api/game/${gameId}/player/${playerId}/location/warehouse/${ goodType }`)
 }
 
 // 3. Gemstone Dealer
-export const actionBuyRuby = () => {
-  axios.post(`api/location/gemstonedealer`)
+export const actionBuyRuby = (gameId, playerId) => {
+  axios.post(`api/game/${gameId}/player/${playerId}/location/gemstonedealer`)
 }
 
 // 4. Markets
-export const actionTradeGoods = (marketSize, fabricNum, fruitNum, gemNum, spiceNum) => {
-  axios.post(`api/location/market/${marketSize}/${fabricNum}/${fruitNum}/${gemNum}/${spiceNum}`)
+export const actionTradeGoods = (gameId, playerId, marketSize, fabricNum, fruitNum, gemNum, spiceNum) => {
+  axios.post(`api/game/${gameId}/player/${playerId}/location/market/${marketSize}/${fabricNum}/${fruitNum}/${gemNum}/${spiceNum}`)
 }
 
 // 5. Mosques
-export const actionBuyMosqueTile = (mosqueSize, selectedTile) => {
-  axios.post(`api/location/mosque/${mosqueSize}/${selectedTile}`)
+export const actionBuyMosqueTile = (gameId, playerId, mosqueSize, selectedTile) => {
+  axios.post(`api/game/${gameId}/player/${playerId}/location/mosque/${mosqueSize}/${selectedTile}`)
 }
 
 // 6. Black Market
-export const actionBlackMarket = (selectedGood, dice1, dice2) => {
-  axios.post(`api/location/blackmarket/${selectedGood}/${dice1}/${dice2}`)
+export const actionBlackMarket = (gameId, playerId, selectedGood, dice1, dice2) => {
+  axios.post(`api/game/${gameId}/player/${playerId}/location/blackmarket/${selectedGood}/${dice1}/${dice2}`)
 }
 
 // 7. Tea house
-export const actionTeaHouse = (number, dice1, dice2) => {
-  axios.post(`api/location/teaHouse/${number}/${dice1}/${dice2}`)
+export const actionTeaHouse = (gameId, playerId, number, dice1, dice2) => {
+  axios.post(`api/game/${gameId}/player/${playerId}/location/teaHouse/${number}/${dice1}/${dice2}`)
 }
 
 // 8. Caravansary
-export const actionGetBonusCard = (type) => {
-  axios.post(`api/location/caravansary/${type}`)
+export const actionGetBonusCard = (gameId, playerId, type) => {
+  axios.post(`api/game/${gameId}/player/${playerId}/location/caravansary/${type}`)
 }
