c0b7fcd828d485f50cab1766482c51a5f7c90f80
Sokmean Nou <sokmean.nou@gmail.com>
2017-03-22 10:48:58 -0400

    updates

--- browser/react/Location/FabricWarehouse.js
+++ browser/react/Location/FabricWarehouse.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import RaisedButton from 'material-ui/RaisedButton';
 
 import Modal from '../Modal/Modal';
 
@@ -11,10 +12,15 @@ class FabricWarehouse extends React.Component {
   }
 
   render() {
+    const style = { margin: 12 }
     return (
       <Modal>
         <div id="location-modal-container">
           <img src={`images/locations/fabric_warehouse.png`} id="img-location" />
+          <p>Look at all the fabric! <br /><br />Your wheelbarrow is now fully loaded with fabric. Come back later if you need more! <br /></p>
+          <div>
+            <RaisedButton label="Ok, end turn." style={style} primary={true}  />
+          </div>
         </div>
       </Modal>
     );
--- browser/react/Location/SpiceWarehouse.js
+++ browser/react/Location/SpiceWarehouse.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import RaisedButton from 'material-ui/RaisedButton';
 
 import Modal from '../Modal/Modal';
 
@@ -11,10 +12,15 @@ class SpiceWarehouse extends React.Component {
   }
 
   render() {
+    const style = { margin: 12 }
     return (
       <Modal>
         <div id="location-modal-container">
           <img src={`images/locations/spice_warehouse.png`} id="img-location" />
+          <p>Look at all the spices! <br /><br />Your wheelbarrow is now fully loaded with spices. Come back later if you need more! <br /></p>
+          <div>
+            <RaisedButton label="Ok, end turn." style={style} primary={true}  />
+          </div>
         </div>
       </Modal>
     );
