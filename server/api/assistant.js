1179143806c8ba90eef8ab1156c326523955a89b
Maria Xia <maria.xia@gmail.com>
2017-03-23 19:50:11 -0400

    added robber picture

--- server/api/assistant.js
+++ server/api/assistant.js
@@ -15,10 +15,32 @@ const router = module.exports = require('express').Router();
  */
 
 // drop off assistant
-router.post('/:coords', (req, res, next, playerId) => {
-  req.playerRef
+router.post('/:coords', (req, res, next) => {
+  const coords = req.params.coords;
+  req.merchantRef.child('assistants').child('count')
+    .transaction(currCount => --currCount)
+    .then(() => {
+      return req.merchantRef.child('assistants').child('out').push(coords);
+    })
+    .then(() => {
+      res.sendStatus(204);
+    })
 });
 
 // pick assistant
-router.post('/:coords', (req, res, next, playerId) => {});
+router.post('/:coords', (req, res, next, playerId) => {
+  const coords = req.params.coords;
+  req.merchantRef.child('assistants').child('count')
+    .transaction(currCount => ++currCount)
+    .then(() => {
+      const remainingAssistantsOut = req.player.assistants.out;
+      assistantsOut.filter(assistantCoords => {
+        return assistantCoords !== coords;
+      })
+      req.merchantRef.child('assistants').child('out').set(remainingAssistantsOut);
+    })
+    .then(() => {
+      res.sendStatus(204);
+    })
+});
 
---
+++ server/api/assistant.test.js
@@ -0,0 +1,27 @@
+const request = require('supertest');
+const express = require('express');
+
+const app = express();
+
+app.get('/user', function(req, res) {
+  res.status(200).json({ name: 'tobi' });
+});
+
+request(app)
+  .get('/user')
+  .expect('Content-Type', /json/)
+  .expect('Content-Length', '15')
+  .expect(200)
+  .end(function(err, res) {
+    if (err) throw err;
+  });
+
+describe('GET /user', function() {
+  it('respond with json', function(done) {
+    request(app)
+      .get('/user')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200, done);
+  });
+});
