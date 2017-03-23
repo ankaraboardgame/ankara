b008c19ce7fcaae5f6d4d5edee4fd2b3cc9304e0
Maria <maria.xia@gmail.com>
2017-03-22 22:39:34 -0400

    Playerroutes (#80)
    * fix(game backend route and game logic): game should store user displaynames
    
    * fix(logic): playerIds and playerMap on game obj
    
    * feat(gemstonedealer): gemstone dealer connected
    
    * feat(end turn and lobby allows you to start game with fewer than 4 players)

--- browser/routes/location.js
+++ browser/routes/location.js
@@ -10,35 +10,35 @@ export const actionBuyWbExt = (gameId, playerId) => {
 
 // 2. Warehouse Action: Max Good
 export const actionMaxGood = (gameId, playerId, goodType) => {
-  return axios.post(`api/game/${gameId}/player/${playerId}/location/warehouse/${ goodType }`)
+  return axios.post(`api/game/${gameId}/player/${playerId}/location/warehouse/${ goodType }`);
 }
 
 // 3. Gemstone Dealer
 export const actionBuyRuby = (gameId, playerId) => {
-  return axios.post(`api/game/${gameId}/player/${playerId}/location/gemstonedealer`)
+  return axios.post(`api/game/${gameId}/player/${playerId}/location/gemstonedealer`);
 }
 
 // 4. Markets
 export const actionTradeGoods = (gameId, playerId, marketSize, fabricNum, fruitNum, gemNum, spiceNum) => {
-  return axios.post(`api/game/${gameId}/player/${playerId}/location/market/${marketSize}/${fabricNum}/${fruitNum}/${gemNum}/${spiceNum}`)
+  return axios.post(`api/game/${gameId}/player/${playerId}/location/market/${marketSize}/${fabricNum}/${fruitNum}/${gemNum}/${spiceNum}`);
 }
 
 // 5. Mosques
 export const actionBuyMosqueTile = (gameId, playerId, mosqueSize, selectedTile) => {
-  return axios.post(`api/game/${gameId}/player/${playerId}/location/mosque/${mosqueSize}/${selectedTile}`)
+  return axios.post(`api/game/${gameId}/player/${playerId}/location/mosque/${mosqueSize}/${selectedTile}`);
 }
 
 // 6. Black Market
 export const actionBlackMarket = (gameId, playerId, selectedGood, dice1, dice2) => {
-  return axios.post(`api/game/${gameId}/player/${playerId}/location/blackmarket/${selectedGood}/${dice1}/${dice2}`)
+  return axios.post(`api/game/${gameId}/player/${playerId}/location/blackmarket/${selectedGood}/${dice1}/${dice2}`);
 }
 
 // 7. Tea house
 export const actionTeaHouse = (gameId, playerId, number, dice1, dice2) => {
-  return axios.post(`api/game/${gameId}/player/${playerId}/location/teaHouse/${number}/${dice1}/${dice2}`)
+  return axios.post(`api/game/${gameId}/player/${playerId}/location/teaHouse/${number}/${dice1}/${dice2}`);
 }
 
 // 8. Caravansary
 export const actionGetBonusCard = (gameId, playerId, type) => {
-  return axios.post(`api/game/${gameId}/player/${playerId}/location/caravansary/${type}`)
+  return axios.post(`api/game/${gameId}/player/${playerId}/location/caravansary/${type}`);
 }
--- browser/routes/player.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-import axios from 'axios';
-
-export const endTurn = (gameId, playerId) => {
-  return axios.post(`/api/game/${gameId}/player/${playerId}/end`)
-};
