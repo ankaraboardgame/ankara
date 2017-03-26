22fd2b42ec50adeb5bd6688c0c40284f279c0d12
Sokmean Nou <sokmean.nou@gmail.com>
2017-03-25 20:50:52 -0400

    udpate(deploy): serviceAccount

--- server/app.js
+++ server/app.js
@@ -10,12 +10,36 @@ const server = http.createServer(app);
 const firebaseAdmin = require('firebase-admin');
 
 // Fetch the service account key JSON file contents
-var productionServiceAccount = require('../firebase.deploy.config.js')
+// var productionServiceAccount = require('../firebase.deploy.config.js')
 // var serviceAccount = require('../secret-istanbul-aa7c8-firebase-adminsdk-inz62-81ab05ed15.json');
+
+const type = process.env.fb_type;
+const project_id = process.env.fb_project_id;
+const private_key_id = process.env.fb_private_key_id;
+const private_key = process.env.fb_private_key;
+const client_email = process.env.fb_client_email;
+const client_id = process.env.fb_client_id;
+const auth_uri = process.env.fb_auth_uri;
+const token_uri = process.env.fb_token_uri;
+const auth_provider_x509_cert_url = process.env.fb_auth_provider_x509_cert_url;
+const client_x509_cert_url = process.env.fb_client_x509_cert_url;
+
+const serviceAccount = {
+	type,
+	project_id,
+	private_key_id,
+	private_key,
+	client_email,
+	client_id,
+	auth_uri,
+	token_uri,
+	auth_provider_x509_cert_url,
+	client_x509_cert_url
+};
 
 // Initialize the app with a service account, granting admin privileges
 firebaseAdmin.initializeApp({
-  credential: firebaseAdmin.credential.cert(productionServiceAccount),
+  credential: firebaseAdmin.credential.cert(serviceAccount),
   databaseURL: 'https://istanbul-aa7c8.firebaseio.com/'
 });
 
