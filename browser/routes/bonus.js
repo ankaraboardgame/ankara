572b79fec0cd1f61e08016622cbb28d44c084a98
Sokmean Nou <sokmean.nou@gmail.com>
2017-03-27 19:38:14 -0400

    bonus cards

--- browser/routes/bonus.js
+++ browser/routes/bonus.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import axios from 'axios';
 
+// only 2 bonus cards at the moment
+
 export const bonusFiveLira = (gameId, playerId) => {
   return axios.post(`api/game/${gameId}/player/${playerId}/bonus/fiveLira`)
 }
