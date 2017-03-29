50d561adc55d71d94e2262129a43e86ce2bff982
Sokmean Nou <sokmean.nou@gmail.com>
2017-03-29 17:20:24 -0400

    update: app.js env vars

--- server/app.js
+++ server/app.js
@@ -40,31 +40,29 @@ if (process.env.NODE_ENV === 'test') {
 	}
 	firebaseAdmin.initializeApp(key);
 } else {
-	console.log('not running test server...');
+	// process.env - config vars set-up on heroku side
+	let serviceAccount = {
+		type: process.env.FIREBASE_TYPE,
+		project_id: process.env.FIREBASE_PROJECT_ID,
+		private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+		private_key: process.env.FIREBASE_PRIVATE_KEY,
+		client_email: process.env.FIREBASE_CLIENT_EMAIL,
+		client_id: process.env.FIREBASE_CLIENT_ID,
+		auth_uri: process.env.FIREBASE_AUTH_URI,
+		token_uri: process.env.FIREBASE_TOKEN_URI,
+		auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+		client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL
+	};
+
+	if(!serviceAccount.private_key) {
+		serviceAccount = require('../secret.firebase.env');
+	}
+
+	firebaseAdmin.initializeApp({
+		credential: firebaseAdmin.credential.cert(serviceAccount),
+		databaseURL: 'https://istanbul-aa7c8.firebaseio.com/'
+	});
 }
-
-// process.env - config vars set-up on heroku side
-let serviceAccount = {
-	type: process.env.FIREBASE_TYPE,
-	project_id: process.env.FIREBASE_PROJECT_ID,
-	private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-	private_key: process.env.FIREBASE_PRIVATE_KEY,
-	client_email: process.env.FIREBASE_CLIENT_EMAIL,
-	client_id: process.env.FIREBASE_CLIENT_ID,
-	auth_uri: process.env.FIREBASE_AUTH_URI,
-	token_uri: process.env.FIREBASE_TOKEN_URI,
-	auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-	client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL
-};
-
-if(!serviceAccount.private_key) {
-  serviceAccount = require('../secret.firebase.env');
-}
-
-firebaseAdmin.initializeApp({
-	credential: firebaseAdmin.credential.cert(serviceAccount),
-	databaseURL: 'https://istanbul-aa7c8.firebaseio.com/'
-});
 
 /** Logging Middleware */
 app.use(morgan('dev'));
