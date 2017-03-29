d9f4310650093a1659fe4171772eb8f75c25a32f
Sokmean Nou <sokmean.nou@gmail.com>
2017-03-29 16:13:59 -0400

    updates: refactored more options, wainwright, comments in the backend location routes

---
+++ browser/utils/options.js
@@ -0,0 +1,39 @@
+import { mapCoordToLocation } from './board';
+
+export function assistantsOutLocations(assistantsOutObj){
+  let assistantsOutLocationsArray = [];
+  for(let assistant in assistantsOutObj){
+    assistantsOutLocationsArray.push(mapCoordToLocation(assistantsOutObj[assistant]).split('_').join(' ').toLowerCase());
+  }
+  return assistantsOutLocationsArray;
+}
+
+export function getPlayerMosqueTiles(playerAbilitiesObj){
+  let tileArray = [];
+  for(let ability in playerAbilitiesObj){
+    if(playerAbilitiesObj[ability].acquired){
+      tileArray.push(playerAbilitiesObj[ability]);
+    }
+  }
+  return tileArray;
+}
+
+export function checkMoneyAndGoods(wheelbarrow, good){
+  return wheelbarrow.money >= 2 ?
+  wheelbarrow[good] < wheelbarrow.size ? true : false
+  : false;
+}
+
+export function checkMoney(money){
+  if(money >= 2) return true;
+  else return false;
+}
+
+export function checkWarehouseCondition(currentPosition){
+  let condition = false;
+  const currentLocation = mapCoordToLocation(currentPosition);
+  if(currentLocation === 'FRUIT_WAREHOUSE' || currentLocation === 'FABRIC_WAREHOUSE' || currentLocation === 'SPICE_WAREHOUSE'){
+    condition = true;
+  }
+  return condition;
+}
