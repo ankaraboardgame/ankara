04f96981cfb593b0cd66cc9a889cb944a4dc744a
Dan Park <danielpark.487@gmail.com>
2017-03-28 21:54:51 -0400

    fix(minor): some design updates

--- browser/react/Footer/RulesSideBar.js
+++ browser/react/Footer/RulesSideBar.js
@@ -23,12 +23,11 @@ export default class SideBar extends Component {
           secondary={ true }
           onTouchTap={this.handleToggle} />
         <Drawer
-          style={style}
           containerStyle={{backgroundColor: 'transparent', display: 'flex', alignItems: 'center', boxShadow: 'none'}}
           width={400}
           openSecondary={true}
           open={this.state.open}>
-          <img src="./images/player_help_card.png" style={{width: '400'}}/>
+          <img src="./images/player_help_card.png" style={{width: '400px'}}/>
         </Drawer>
       </div>
     );
